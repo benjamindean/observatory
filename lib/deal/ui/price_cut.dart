d76cfab3796aa8a5624f5ea492ef43a5ce8b8bfe
Benjamin Dean <berman.reshetnikov@gmail.com>
2024-05-30 12:19:11 +0300

    Tweak colors and add tests

--- lib/deal/ui/price_cut.dart
+++ lib/deal/ui/price_cut.dart
@@ -57,7 +57,7 @@ class PriceCut extends StatelessWidget {
     }
 
     final Color highlightColor = HSLColor.fromColor(
-      context.colors.scheme.primary.darken(15),
+      context.colors.scheme.primary.darken(10),
     ).withSaturation((priceCutInt / 100).clamp(0.4, 1.0)).toColor();
 
     return Row(
